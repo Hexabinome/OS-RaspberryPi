2f9fd79d08cae3053b3d63e7f0381926be4377e0
David W <david.wobrock@gmail.com>
2015-12-08 09:05:24 +0100

    working MMU :D

--- kmain.c
+++ kmain.c
@@ -6,8 +6,13 @@ void kmain()
 {
 	sched_init();
 	
-	 __asm("cps 0x10");
+	 //__asm("cps 0x10"); // CPU to USER mode
 
-	vmem_translate(0, NULL);
-	vmem_translate(1, NULL);
+	uint32_t a = vmem_translate(0, NULL);
+	uint32_t b = vmem_translate(1, NULL);
+	uint32_t c = vmem_translate(0x10111111, NULL);
+	
+	a++;
+	b++;
+	c++;
 }
