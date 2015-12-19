583420311d2886a4a741ca2ccd8278147b0c9a76
David Wobrock <david.wobrock@gmail.com>
2015-12-19 20:27:41 +0100

    add python script which launches all tests (no regression framework)

---
+++ tools/run-all-tests.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from sys import stdout
+from subprocess import check_output
+from time import time
+
+def run_all_tests():
+	for test in tests:
+		kmain_file = test[0]
+		gdb_file = test[1]
+		
+		command_line = "./run-test.sh ../test/{0} ../test/{1}".format(kmain_file, gdb_file)
+		print("Test {0}".format(command_line), end=" : ")
+		stdout.flush()
+		start_time = time()
+		test_output = check_output(command_line, shell=True).decode(encoding='UTF-8')
+		exec_time = time() - start_time
+		
+		if test_output.endswith("OK\n"):
+			print("OK ({0}s)".format(exec_time))
+		elif test_output.endswith("ERROR\n"):
+			print("ERROR ({0}s)".format(exec_time))
+		else:
+			print("ERROR (unexpected) ({0}s)".format(exec_time))
+
+
+
+tests = [
+	["kmain-bidule.c", "bidule-called-twice.gdb"],
+	["kmain-gettime.c", "sys-gettime-returns-value.gdb"],
+	["kmain-settime.c", "sys-settime-passes-argument.gdb"],
+	["kmain-reboot.c", "sys-reboot-does-reboot.gdb"],
+	["kmain-nop-reboot.c", "sys-nop-does-return.gdb"],
+	["kmain-nop-reboot.c", "swi-handler-preserves-SP.gdb"],
+# No yieldto tests because create_process returned the process pcb
+#	["kmain-yieldto.c", "sys-yieldto-jumps-to-dest.gdb"],
+#	["kmain-yieldto.c", "sys-yieldto-preserves-locals.gdb"],
+#	["kmain-yieldto.c", "sys-yieldto-preserves-status-register.gdb"],
+# No yield test, because now we have IRQ interruptions
+#	["kmain-yield.c", "round-robin-distinct-stacks.gdb"],
+#	["kmain-yield.c", "round-robin-fairness.gdb"],
+	["kmain-pagination.c", "pagination.gdb"],
+	["kmain-vmem-alloc-free.c", "vmem-alloc-free.gdb"],
+	["kmain-mmap-munmap.c", "sys-mmap-munmap.gdb"]
+]
+
+if __name__ == "__main__":
+	run_all_tests()
