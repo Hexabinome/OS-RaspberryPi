b987495f36f968b048ecbfacc7ba1e17f4088df0
David W <david.wobrock@gmail.com>
2016-01-04 15:44:14 +0100

    creating files

--- kmain.c
+++ kmain.c
@@ -1,41 +1,12 @@
-#include "syscall.h"
 #include "sched.h"
-#include "pwm.h"
-
-int process()
-{
-	int32_t cpt = 10;
-	
-	int pid = sys_fork();
-	if (pid == -1)
-	{
-		// failed
-	}
-	else if (pid == 0)
-	{
-		// Child
-		cpt--;
-		while (1) ;
-	}
-	else
-	{
-		// parent
-		cpt++;
-		while (1) ;
-	}
-	
-	return 0;
-}
+#include "shell.h"
 
 void kmain()
 {
 	sched_init();
-	
-	create_process(&process);
-	
 	irq_init();
 	
 	__asm("cps 0x10"); // CPU to USER mode
 	
-	while (1) ;
+	start_shell();
 }
---
+++ src/shell.c
@@ -0,0 +1,5 @@
+#include "shell.h"
+
+void start_shell()
+{
+}
